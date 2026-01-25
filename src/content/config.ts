02bac4c1fdb4393975b56d71fa18900cca023199
thesmartgadjet-stack <thesmartgadjet@gmail.com>
2026-01-25 07:13:04 -0800

    fg

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,14 +3,18 @@ const postsCollection = defineCollection({
   schema: ({ image }) =>
     z.object({
       title: z.string(),
-      pubDate: z.date(),
+      pubDate: z.union([z.date(), z.string()]).transform((str) => new Date(str)),
       description: z.string(),
       author: z.string(),
-      image: z.object({
-        url: image(),
-        alt: z.string(),
-      }),
-      tags: z.array(z.string()),
+      image: z.union([
+        z.string(),
+        z.object({
+          url: image(),
+          alt: z.string(),
+        }),
+      ]),
+      tags: z.array(z.string()).optional().default([]),
+      category: z.string().optional(),
     }),
 });
 export const collections = {
